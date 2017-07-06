74cc8627196f4dd1b73f2c8140c5e300193065fc
overlord2304 <overlord2304@gmail.com>
2017-07-06 14:30:58 +0200

    Started algo
    Started turning the pseudo code previously marked as 'algorithm' into actual code.

--- Python UI/calc_rest.py
+++ Python UI/calc_rest.py
@@ -167,9 +167,16 @@ def list(colorCombo, pos)
 	# List algo 7 """
 
 
-# Algorithm
-	# Check if the cube is solved, simple Boolean TRUE / FALSE (LOOP)
-		# Check to see if the white edges are solved, done through a simple Boolean TRUE / FALSE (LOOP)
+def Algorithm
+	solved = False
+	algo1 = False
+	algo2 = False
+	algo3 = False
+	algo4 = False
+	algo5 = False
+	algo6 = False
+	while solved == False: # Check if the cube is solved, simple Boolean TRUE / FALSE (LOOP)
+		while algo1 == False:# Check to see if the white edges are solved, done through a simple Boolean TRUE / FALSE (LOOP)
 			# Check each block asociated with a edge to see if it is white
 				# If a white edge is found and it is already in, or has been moved to a correct position then ignore it.
 					# Increase a counter that keeps track of the amount of correct edges.
@@ -178,7 +185,7 @@ def list(colorCombo, pos)
 					# Take the number asociated with the white block and run it through a list to check what moves should be performed to get it into it's proper position.
 						# Store these moves in moveListBuffer
 				# Move to the next edge block
-		# Check to see if the white face is solved, simple Boolean TRUE / FALSE (LOOP)
+		while algo2 == False: # Check to see if the white face is solved, simple Boolean TRUE / FALSE (LOOP)
 			# Check each corner block for the color white
 				# If a white Corner is found and it is already in, or has been moved to a correct position then ignore it.
 					# Increase a counter that keeps track of the amount of correct corners.
@@ -186,7 +193,7 @@ def list(colorCombo, pos)
 				# Check the two other blocks of the corner for their color
 					# Take the number asociated with the white block and run it through a list to see what moves should be performed to get it into it's proper position.
 						# Store these moves in moveListBuffer.
-		# Check to see if the middle layer is solved, simple Boolean TRUE / FALSE (LOOP) @##$U*@$*(!@($#!U@$ !@#$!@*U$NJSAU*!&$(!@#(!@#NCAISKDQ !@#*@$!@&(#*SDAS DAS d)) CHECK IF THIS BLOCK WORKS (IN THEORY ANYWAYS)
+		while algo3 == False: # Check to see if the middle layer is solved, simple Boolean TRUE / FALSE (LOOP) @##$U*@$*(!@($#!U@$ !@#$!@*U$NJSAU*!&$(!@#(!@#NCAISKDQ !@#*@$!@&(#*SDAS DAS d)) CHECK IF THIS BLOCK WORKS (IN THEORY ANYWAYS)
 			# Check each edge on the middle and back layer of the cube for a orange color
 				# If a orange edge is found then check the other side of the edge 
 					# Check if the block is in the correct position
@@ -195,7 +202,7 @@ def list(colorCombo, pos)
 							# If the counter indicates 3, meaning all four edges are in the correct position then break out of this loop and move onto the next part of the algorithm after setting a Boolean to TRUE
 						# Else check the color combination in combination with it's position in the list.
 							# Store the corresponding moves in moveListBuffer
-		# Check to see if the yellow cross exists, simple Boolean TRUE / FALSE (LOOP)
+		while algo4 == False: # Check to see if the yellow cross exists, simple Boolean TRUE / FALSE (LOOP)
 			#Check each edge that is still not in the correct position (Back) for yellow
 				# Check if the edge is in the correct position already
 					# If the block is in the correct position then mark it as completed (Can be one of four spots. aslong as it is at a edge on the yellow face it counts as a correct position)
@@ -203,7 +210,7 @@ def list(colorCombo, pos)
 						# If the counter indicates 3, meaning all four edges are in the correct position then break out of this loop and move onto the next part of the algorithm after setting a Boolean to TRUE
 					# Else go through the list using the position of the yellow edge.
 						# Store the corresponding moves required to put the edge in the correct position in moveListBuffer
-		# Check to see if the yellow edges are solved, simple Boolean TRUE / FALSE (LOOP)
+		while algo5 == False # Check to see if the yellow edges are solved, simple Boolean TRUE / FALSE (LOOP)
 			# Check the color of the color on the non yellow sides of the edges.
 				# If the color matches the color of the center of the face (5)
 					# Increase a counter that keeps track of the amount of correct edges.
@@ -213,7 +220,7 @@ def list(colorCombo, pos)
 						# Go back to looking for a unsolved block skipping this one for the next round
 					# Else go through the list 
 						# Store the moves in moveListBuffer #@!$!*@$*!@)#!@$(!@$)!@)#!@)_%()!@# KEEP IN MIND THAT THIS DOES NOT FOLLOWS THE STANDARD ALGORITHM AND IT NEEDS TO BE ADAPTED TO WORK FOR EACH OF THE FOUR SIDES!
-		# Check to see if the yellow corners are prepared correctly, simple Boolean TRUE / FALSE (LOOP)
+		while algo6 == False # Check to see if the yellow corners are prepared correctly, simple Boolean TRUE / FALSE (LOOP)
 			# Check if the side on the yellow face is NOT yellow
 				# If the other colors on the corner DO NOT match the color at the center of their respective face
 					# Go into the list using the position of this corner
@@ -221,7 +228,7 @@ def list(colorCombo, pos)
 							# Store the attached moves in moveListBuffer as many times as is needed to react a correct cube state
 							# Set the Boolean to TRUE and break out of this loop.
 			# Else move onto the next corner
-		# Check to see if the cube is solved, simple Boolean TRUE / FALSE (LOOP)
+		while solved == False # Check to see if the cube is solved, simple Boolean TRUE / FALSE (LOOP)
 			# Check if all faces only contain one color 
 				# Set the Boolean to TRUE and break out of this loop
 			# Else check which yellow corners are placed incorrectly
