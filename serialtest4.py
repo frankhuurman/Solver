e1dc1e1fea38c3f166c4225e8f2001cde4d812fa
Frank Huurman <fhuurman@REA.ICT>
2017-07-10 14:14:57 +0200

    Added input variable for Strings
    We can input our own strings from the serialtest4.py script now which will be sent to Arduino.
    Arduino now has a byte array in which it collects all the incoming bytes and prints it to the serial port.
    
    There's probably a max of 512 bytes of strings to send so keep that in mind!

--- serialtest4.py
+++ serialtest4.py
@@ -4,18 +4,16 @@ ser = serial.Serial('COM3', 9600, timeout=2)  # open serial port
 print(ser.name)         # check which port was really used
 #ser.write(b'hello')     # write a string converted to bytes
 
-def sendThing():
-	ser.write(b"yes")
-
 while True:
-	data = ser.readline()[:-2] #the last bit gets rid of the new-line chars
+	data = ser.readline() #the last bit gets rid of the new-line char
 	#print (data)
 	#print (data)
 	if data:
-		if data == b"Ready":
+		if data == b"Ready\r\n":
 			print ("hi")
-			input("send hello string now?")
-			ser.write(b"hello")
+			arduino_string = input("send hello string now?")
+			arduino_send_bytes = str.encode(arduino_string)
+			ser.write(arduino_send_bytes)
 		elif data == b'hello':
 			print ("it says hello!")
 		else:
@@ -24,7 +22,6 @@ while True:
 			#ser.write(b'hello')
 
 	if not data:
-		sendThing()
 		print("No data being received from Arduino anymore")
 		break
 
--- sketch_jun27a/sketch_jun27a.ino
+++ sketch_jun27a/sketch_jun27a.ino
@@ -12,7 +12,8 @@ if (Serial.available()>0) { // there are bytes in the serial buffer to read
       // from the serial buffer so keep reading the buffer until all the bytes 
       // have been read.
          myByte = Serial.read(); // read in the next byte
-         Serial.write(myByte); // ASCII character
+         Serial.print(myByte);
+         //Serial.write(myByte); // ASCII character
       }
       Serial.println(); // carriage return
       delay(100); // a short delay
