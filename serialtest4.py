a829aa575598b2a64895ccc3078502e0e14b55cb
Frank <huurmanfrank@gmail.com>
2017-07-24 14:51:13 +0200

    Limiet versturen test
    959 karakters in 1 keer versturen naar Arduino is de limiet.

--- serialtest4.py
+++ serialtest4.py
@@ -3,13 +3,34 @@ from time import sleep
 ser = serial.Serial("COM3", 9600, timeout=2)  # Open serial port
 print("Port used: " + ser.name)         # Check which port was really used
 
+testlist = ["r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
+			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/"]
+testlist.append("\r")
 while True:
 	data = ser.readline() # Read data from Arduino
 	if data: # If data comes in from Arduino
 		if data == b"Ready\r\n": # Initialize handshake with Arduino
 			print ("Handshake from Arduino received")
-			arduino_string = input("Type string to send to arduino: ")
-			ser.write(str.encode(arduino_string + "\r"))
+			#arduino_string = input("Type string to send to arduino: ")
+			for item in testlist:
+				ser.write(str.encode(item))
 		elif data == b"somethingelse":
 			arduino_string = input("Type another string to send to arduino: ")
 			arduino_send_bytes = str.encode(arduino_string)
