76a22562b0dde14d5c6fca0a835c61520789853c
Frank <huurmanfrank@gmail.com>
2017-07-25 09:35:12 +0200

    Changed list name, removed limit test
    All the characters used for testing are not needed anymore so I've deleted those, changed list name to send_list.

--- serialtest4.py
+++ serialtest4.py
@@ -3,33 +3,15 @@ from time import sleep
 ser = serial.Serial("COM3", 9600, timeout=2)  # Open serial port
 print("Port used: " + ser.name)         # Check which port was really used
 
-testlist = ["r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/", \
-			"r", "e", "a", "c", "o", "l", "l", "e", "g", "e", "/"]
-testlist.append("\r")
+send_list = []
+send_list.append("\r")
 while True:
 	data = ser.readline() # Read data from Arduino
 	if data: # If data comes in from Arduino
 		if data == b"Ready\r\n": # Initialize handshake with Arduino
 			print ("Handshake from Arduino received")
 			#arduino_string = input("Type string to send to arduino: ")
-			for item in testlist:
+			for item in send_list:
 				ser.write(str.encode(item))
 		elif data == b"somethingelse":
 			arduino_string = input("Type another string to send to arduino: ")
