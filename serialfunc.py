e5aaa371b8112fb0167c963718dde4153ef0a8c2
Frank <huurmanfrank@gmail.com>
2017-07-26 12:05:01 +0200

    function to send to arduino
    merge this with the part size of 50 :+1:

---
+++ serialfunc.py
@@ -0,0 +1,33 @@
+def sendToArduino(translatedlist):
+
+	ser = serial.Serial("COM4", 9600, timeout=2)  # Open serial port
+	print("Port used: " + ser.name)         # Check which port was really used
+
+	translatedlist.append("\r")
+
+	while True:
+		data = ser.readline() # Read data from Arduino
+		if data: # If data comes in from Arduino
+			if data == b"Ready\r\n": # Initialize handshake with Arduino
+				print ("Handshake from Arduino received")
+				for item in translatedlist:
+					ser.write(str.encode(item))
+			elif data == b"somethingelse":
+				arduino_string = input("Type another string to send to arduino: ")
+				arduino_send_bytes = str.encode(arduino_string)
+				ser.write(arduino_send_bytes)
+			elif data == b'hello':
+				print ("it says hello!")
+			else:
+				# This actually prints the data received from Serial.print from Arduino
+				# First it decodes the received raw byte data to a utf-8 string
+				ascii_data = data.decode()
+				print (ascii_data)
+
+		if not data: # If there is no data coming back from Arduino
+			print("No data being received from Arduino anymore")
+			break
+
+	test = input("Close?")
+	ser.close()             # close port
+	print ("Serial port closed")
