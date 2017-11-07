3053b15810ee4f39a7f8cc3b2af5ec0ed374c1fa
Joost2S <joost.j2s@gmail.com>
2017-11-07 11:58:45 +0100

    Testfile for writing debug info

--- Python UI/test.py
+++ Python UI/test.py
@@ -1,64 +1,14 @@
 
-import cube as kubus
-import random
-import sys
-import time
+
+import datetime
+
+def write(args):
+	debug = open("debug.txt", "a", newline = "\r\n") # opens debug.txt
+	debug.write(str(args) + "\t") # writes all the info on te defective moves to debug.txt
+	debug.write(str(datetime.datetime.now()) + "\n") # adds a timestamp
+	debug.close() # closes debug.txt
 
 
-dinges = "rggrrwyyoyggrwbbboryyoobwwbggwyyboobwwgrboryyboorggrww"
-print(len(dinges))
-moves = []
-blah = ["D", "L", "B", "U", "R", "F"]
-for i in blah:
-	moves.append(i)
-	moves.append(i.lower())
-cube = kubus.cube("r"*9 + "w"*9 + "o"*9 + "y"*9 + "b"*9 + "g"*9)
-t = 0
-if (cube.solved()):
-	print("moi")
-
-#while(True):
-i = 0
-m = ""
-x = time.time()
-t = time.time()
-for a in moves:
-	for b in moves:
-		for c in moves:
-			for d in moves:
-				for e in moves:
-					for f in moves:
-						cube.setStart()
-						cube.sendMoves(a+b+c+d+e+f)
-						i += 1
-						colors = ""
-						for face in cube.facenames:
-							colors += cube.faces[face].getColors()
-						if (i >= 10**4):
-							i = 0
-							print(a+b+c+d+e+f)
-							print(colors)
-							print(dinges)
-							print((10 ** 4) / (time.time() - t))
-							t = time.time()
-						if (colors == dinges):
-							print(m)
-							print(time.time() - x)
-							sys.exit()
-print(time.time() - x)
-"""
-	for b in range(10 ** 4):
-		m = ""
-		cube.setStart()
-		for i in range(6):
-			move = moves[int(random.random() * 6)]
-			if (random.random()*1 < 0.5):
-				move = move.lower()
-			cube.sendMoves(move)
-			m += move
-"""
-"""
-#		else:
-#			print(m)
-	print(m)
-"""
+write("moi")
+write("blah")
+write("Piep")
